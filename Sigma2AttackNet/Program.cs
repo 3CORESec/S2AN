9da37fde6775375e7056829ee0b6e563a2ae6ab5
pdr9rc <pedro.gracio@3coresec.com>
2021-01-11 14:43:33 +0000

    added  tcp-stream and tcp-pkt

--- Sigma2AttackNet/Program.cs
+++ Sigma2AttackNet/Program.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+using System.Net;
 using CommandLine;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using YamlDotNet.Core;
 using Parser = CommandLine.Parser;
 
@@ -14,6 +15,10 @@ namespace Sigma2AttackNet
     {
         public class Options
         {
+
+            [Option("category-url", Required = false, HelpText = "Url with mitre matrix")]
+            public string CategoryMatrix { get; set; } = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json";
+
             [Option('d', "rules-directory", Required = true, HelpText = "Directory to read rules from")]
             public string RulesDirectory { get; set; }
 
@@ -22,15 +27,45 @@ namespace Sigma2AttackNet
 
             [Option("no-comment", Required = false, HelpText = "Don't store rule names in comments")]
             public bool NoComment { get; set; } = false;
+
+            [Option("no-warning", Required = false, HelpText = "Don't check techniques")]
+            public bool NoWarning { get; set; } = false;
         }
         public static void Main(string[] args)
         {
+            var warnings = new List<string>();
             Dictionary<string, List<string>> techs = new Dictionary<string, List<string>>();
             int ruleCount = 0;
             int gradientMax = 0;
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(o =>
                 {
+
+                Dictionary<string, List<string>> matrix = new Dictionary<string, List<string>>();
+                    if (!o.NoWarning)
+                    {
+                        var _matrix = (JsonConvert.DeserializeObject<JObject>(new WebClient().DownloadString(o.CategoryMatrix))["objects"] as JArray)!
+                            .Where(x => x["external_references"] != null && x["external_references"].Any(y => y["source_name"] != null && x["kill_chain_phases"] != null));
+                        
+                        foreach (var x in _matrix)
+                        {
+
+                            var techId = x["external_references"]
+                                .First(x => x["source_name"].ToString() == "mitre-attack")["external_id"]
+                                .ToString();
+
+                            if (!matrix.ContainsKey(techId))
+                            matrix.Add(techId,
+                                x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString()).ToList()
+                            );
+                            else
+                            {
+                                matrix[techId] = matrix[techId].Concat(x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString())).ToList();
+                            }
+                        };
+    
+                    }
+
                     foreach (var ruleFile in Directory.EnumerateFiles(o.RulesDirectory, "*.yml", SearchOption.AllDirectories))
                     {
                         try
@@ -42,6 +77,18 @@ namespace Sigma2AttackNet
                                 continue;
                             }
 
+                            string title = null;
+
+                            if (!o.NoWarning)
+                                try 
+                                { 
+                                    title = new string(contents.TakeWhile(c => c != '\n').ToArray()).Split(":")[1];
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("unable to parse a rule title");
+                                }
+
                             contents = contents.Replace(Environment.NewLine + Environment.NewLine,
                                         Environment.NewLine)
                                     .Remove(0, contents.IndexOf("tags", StringComparison.Ordinal));
@@ -52,6 +99,7 @@ namespace Sigma2AttackNet
                             if (dict.ContainsKey("tags"))
                             {
                                 var tags = dict["tags"];
+                                string category = null;
                                 foreach (string tag in tags)
                                 {
                                     if (tag.ToLower().StartsWith("attack.t"))
@@ -63,6 +111,27 @@ namespace Sigma2AttackNet
                                         techs[techniqueId].Add(ruleFile.Split("\\").Last());
                                         if (techs.Count > gradientMax)
                                             gradientMax = techs.Count;
+
+                                        if (category != null && !o.NoWarning)
+                                        {
+                                            try
+                                            {
+                                                if (!(matrix.ContainsKey(techniqueId) && matrix[techniqueId].Contains(category)))
+                                                    warnings.Add($"MITRE technique ({techniqueId}) and tactic ({category}) mismatch in rule: {title}");
+                                            }
+                                            catch (Exception)
+                                            {
+
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        category = tag.Replace("attack.", "")
+                                        .Replace("_", "-")
+                                        .ToLower();
+
+
                                     }
                                 }
                             }
@@ -98,6 +167,7 @@ namespace Sigma2AttackNet
                         NullValueHandling = NullValueHandling.Ignore
                     }));
                     Console.WriteLine($"[*] Layer file written in {o.OutFile} ({ruleCount} rules)");
+                    warnings.ForEach(Console.WriteLine);
                 });
         }
     }
