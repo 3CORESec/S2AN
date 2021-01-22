997785e450d0cb0d72c245c4e437138a27f5cc9d
pdr9rc <pedro.gracio@3coresec.com>
2021-01-22 19:08:39 +0000

    commit messup fix and new version
    two birds with one stone kind of commit...

--- Sigma2AttackNet/Program.cs
+++ Sigma2AttackNet/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using CommandLine;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -15,57 +16,57 @@ namespace Sigma2AttackNet
     {
         public class Options
         {
-
             [Option("category-url", Required = false, HelpText = "Url with mitre matrix")]
-            public string CategoryMatrix { get; set; } = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json";
-
+            public string CategoryMatrix { get; set; }
             [Option('d', "rules-directory", Required = true, HelpText = "Directory to read rules from")]
             public string RulesDirectory { get; set; }
-
             [Option('o', "out-file", Required = false, HelpText = "File to write the JSON layer to")]
             public string OutFile { get; set; } = "sigma-coverage.json";
-
             [Option("no-comment", Required = false, HelpText = "Don't store rule names in comments")]
             public bool NoComment { get; set; } = false;
-
-            [Option("no-warning", Required = false, HelpText = "Don't check techniques")]
-            public bool NoWarning { get; set; } = false;
+            [Option('w', "warning", Required = false, HelpText = "Check for ATT&CK technique and tactic mismatch")]
+            public bool Warning { get; set; } = false;
         }
         public static void Main(string[] args)
         {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceStream = assembly.GetManifestResourceStream("Sigma2AttackNet.config.json");
+            StreamReader reader = new StreamReader(resourceStream);
+            JObject temp = JsonConvert.DeserializeObject<JObject>(reader.ReadToEnd());
             var warnings = new List<string>();
             Dictionary<string, List<string>> techs = new Dictionary<string, List<string>>();
             int ruleCount = 0;
             int gradientMax = 0;
+            Console.WriteLine(" ");
+            Console.WriteLine($"S2AN by: 3CORESec - {temp["repo_url"]}");
+            Console.WriteLine(" ");
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(o =>
                 {
-
-                Dictionary<string, List<string>> matrix = new Dictionary<string, List<string>>();
-                    if (!o.NoWarning)
+                    Dictionary<string, List<string>> matrix = new Dictionary<string, List<string>>();
+                    if (o.CategoryMatrix == null)
+                    {
+                        o.CategoryMatrix = temp["category_matrix_url"]?.ToString();
+                    }
+                    if (o.Warning)
                     {
                         var _matrix = (JsonConvert.DeserializeObject<JObject>(new WebClient().DownloadString(o.CategoryMatrix))["objects"] as JArray)!
                             .Where(x => x["external_references"] != null && x["external_references"].Any(y => y["source_name"] != null && x["kill_chain_phases"] != null));
-                        
                         foreach (var x in _matrix)
                         {
-
                             var techId = x["external_references"]
                                 .First(x => x["source_name"].ToString() == "mitre-attack")["external_id"]
                                 .ToString();
-
                             if (!matrix.ContainsKey(techId))
-                            matrix.Add(techId,
-                                x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString()).ToList()
-                            );
+                                matrix.Add(techId,
+                                    x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString()).ToList()
+                                );
                             else
                             {
                                 matrix[techId] = matrix[techId].Concat(x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString())).ToList();
                             }
-                        };
-    
+                        }
                     }
-
                     foreach (var ruleFile in Directory.EnumerateFiles(o.RulesDirectory, "*.yml", SearchOption.AllDirectories))
                     {
                         try
@@ -76,20 +77,8 @@ namespace Sigma2AttackNet
                                 Console.WriteLine($"Ignoring rule {ruleFile} (no tags)");
                                 continue;
                             }
-
-                            string title = null;
-
-                            if (!o.NoWarning)
-                                try 
-                                { 
-                                    title = new string(contents.TakeWhile(c => c != '\n').ToArray()).Split(":")[1];
-                                }
-                                catch (Exception e)
-                                {
-                                    Console.WriteLine("unable to parse a rule title");
-                                }
-
-                            contents = contents.Replace(Environment.NewLine + Environment.NewLine,
+                            if (o.Warning)
+                                contents = contents.Replace(Environment.NewLine + Environment.NewLine,
                                         Environment.NewLine)
                                     .Remove(0, contents.IndexOf("tags", StringComparison.Ordinal));
                             if (contents.Contains("---"))
@@ -112,16 +101,16 @@ namespace Sigma2AttackNet
                                         if (techs.Count > gradientMax)
                                             gradientMax = techs.Count;
 
-                                        if (category != null && !o.NoWarning)
+                                        if (category != null && o.Warning)
                                         {
                                             try
                                             {
                                                 if (!(matrix.ContainsKey(techniqueId) && matrix[techniqueId].Contains(category)))
-                                                    warnings.Add($"MITRE technique ({techniqueId}) and tactic ({category}) mismatch in rule: {title}");
+                                                    warnings.Add($"MITRE ATT&CK technique ({techniqueId}) and tactic ({category}) mismatch in rule: {ruleFile.Split("\\").Last()}");
                                             }
                                             catch (Exception)
                                             {
-
+                                                // ignored
                                             }
                                         }
                                     }
@@ -130,8 +119,6 @@ namespace Sigma2AttackNet
                                         category = tag.Replace("attack.", "")
                                         .Replace("_", "-")
                                         .ToLower();
-
-
                                     }
                                 }
                             }
@@ -149,7 +136,6 @@ namespace Sigma2AttackNet
                             score = entry.Value.Count,
                             comment = (o.NoComment) ? null : string.Join(Environment.NewLine, entry.Value.Select(x => x.Split("/").Last()))
                         });
-
                     File.WriteAllText(o.OutFile.EndsWith(".json") ? o.OutFile : $"{o.OutFile}.json", JsonConvert.SerializeObject(new
                     {
                         domain = "mitre-enterprise",
@@ -160,13 +146,18 @@ namespace Sigma2AttackNet
                             maxValue = gradientMax,
                             minValue = 0
                         },
-                        version = "3.0",
+                        version = "4.1",
                         techniques = entries
                     }, Formatting.Indented, new JsonSerializerSettings
                     {
                         NullValueHandling = NullValueHandling.Ignore
                     }));
                     Console.WriteLine($"[*] Layer file written in {o.OutFile} ({ruleCount} rules)");
+                    if (warnings.Any())
+                    {
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Attention - mismatch between technique and tactic has been detected!");
+                    }
                     warnings.ForEach(Console.WriteLine);
                 });
         }
