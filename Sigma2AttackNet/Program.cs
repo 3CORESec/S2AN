83f7657e3a6537b7724a18bd1fa015887493890e
pdr9rc <pedro.gracio@3coresec.com>
2021-07-08 16:38:33 +0100

    fixes to mismatch logic and code cleanup

--- Sigma2AttackNet/Program.cs
+++ Sigma2AttackNet/Program.cs
@@ -17,7 +17,7 @@ namespace Sigma2AttackNet
         public class Options
         {
             [Option('c',"category-url", Required = false, HelpText = "URL with MITRE matrix")]
-            public string CategoryMatrix { get; set; }
+            public string MitreMatrix { get; set; }
             [Option('d', "rules-directory", Required = true, HelpText = "Directory to read rules from")]
             public string RulesDirectory { get; set; }
             [Option('o', "out-file", Required = false, HelpText = "File to write the JSON layer to")]
@@ -27,108 +27,78 @@ namespace Sigma2AttackNet
             [Option('w', "warning", Required = false, HelpText = "Check for ATT&CK technique and tactic mismatch")]
             public bool Warning { get; set; } = false;
         }
+        //Matrix to search for mismatches in case of w option
+        static Dictionary<string, List<string>> mismatchSearchMatrix = new Dictionary<string, List<string>>();
+        //List of mismatch warnings
+        static List<string> mismatchWarnings = new List<string>();
+        //List of techniques
+        static Dictionary<string, List<string>> techniques = new Dictionary<string, List<string>>();
+        /// <summary>
+        /// for each file that contains tags, adds the file name to the techniques map for key technique.
+        /// Also if warning flag is on, searches, if any, a given category group ON TOP of a single OR group of techniques.
+        /// these category groups are reset whenever a new technique category entry is found, and this new group will be applied to the following technique group and so forth.
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceStream = assembly.GetManifestResourceStream("Sigma2AttackNet.config.json");
-            StreamReader reader = new StreamReader(resourceStream);
-            JObject temp = JsonConvert.DeserializeObject<JObject>(reader.ReadToEnd());
-            var warnings = new List<string>();
-            Dictionary<string, List<string>> techs = new Dictionary<string, List<string>>();
+            
             int ruleCount = 0;
             int gradientMax = 0;
-            Console.WriteLine(" ");
-            Console.WriteLine($"S2AN by 3CORESec - {temp["repo_url"]}");
-            Console.WriteLine(" ");
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(o =>
                 {
-                    Dictionary<string, List<string>> matrix = new Dictionary<string, List<string>>();
-                    if (o.CategoryMatrix == null)
-                    {
-                        o.CategoryMatrix = temp["category_matrix_url"]?.ToString();
-                    }
-                    if (o.Warning)
-                    {
-                        var _matrix = (JsonConvert.DeserializeObject<JObject>(new WebClient().DownloadString(o.CategoryMatrix))["objects"] as JArray)!
-                            .Where(x => x["external_references"] != null && x["external_references"].Any(y => y["source_name"] != null && x["kill_chain_phases"] != null));
-                        foreach (var x in _matrix)
-                        {
-                            var techId = x["external_references"]
-                                .First(x => x["source_name"].ToString() == "mitre-attack")["external_id"]
-                                .ToString();
-                            if (!matrix.ContainsKey(techId))
-                                matrix.Add(techId,
-                                    x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString()).ToList()
-                                );
-                            else
-                            {
-                                matrix[techId] = matrix[techId].Concat(x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString())).ToList();
-                            }
-                        }
-                    }
-                    foreach (var ruleFile in Directory.EnumerateFiles(o.RulesDirectory, "*.yml", SearchOption.AllDirectories))
+                    LoadConfig(o);
+                    LoadMismatchSearchMatrix(o);
+                    foreach (var ruleFilePath in Directory.EnumerateFiles(o.RulesDirectory, "*.yml", SearchOption.AllDirectories))
                     {
                         try
                         {
-                            string contents = File.ReadAllText(ruleFile);
-                            if (!contents.Contains("tags"))
-                            {
-                                Console.WriteLine($"Ignoring rule {ruleFile} (no tags)");
-                                continue;
-                            }
-                            if (o.Warning)
-                                contents = contents.Replace(Environment.NewLine + Environment.NewLine,
-                                        Environment.NewLine)
-                                    .Remove(0, contents.IndexOf("tags", StringComparison.Ordinal));
-                            if (contents.Contains("---"))
-                                contents = contents.Remove(contents.IndexOf("---", StringComparison.Ordinal));
-                            var deserializer = new YamlDotNet.Serialization.Deserializer();
-                            var dict = deserializer.Deserialize<Dictionary<string, dynamic>>(contents);
-                            if (dict.ContainsKey("tags"))
+                            var dict = DeserializeYamlFile(ruleFilePath, o);
+                            if (dict != null && dict.ContainsKey("tags"))
                             {
                                 ruleCount++;
                                 var tags = dict["tags"];
-                                string category = null;
+                                var categories = new List<string>();
+                                string lastEntry = null;
                                 foreach (string tag in tags)
                                 {
+                                    //If its the technique id entry, then this adds the file name to the techniques map
                                     if (tag.ToLower().StartsWith("attack.t"))
                                     {
                                         var techniqueId = tag.Replace("attack.", "").ToUpper();
-                                        if (!techs.ContainsKey(techniqueId))
-                                            techs[techniqueId] = new List<string>();
-                                        techs[techniqueId].Add(ruleFile.Split("\\").Last());
-                                        if (techs.Count > gradientMax)
-                                            gradientMax = techs.Count;
-
-                                        if (category != null && o.Warning)
+                                        if (!techniques.ContainsKey(techniqueId))
+                                            techniques[techniqueId] = new List<string>();
+                                        techniques[techniqueId].Add(ruleFilePath.Split("\\").Last());
+                                        if (techniques.Count > gradientMax)
+                                            gradientMax = techniques.Count;
+                                        //then if there are any categories so far, it checks for a mismatch for each one
+                                        if (categories.Count > 0 && o.Warning)
                                         {
-                                            try
-                                            {
-                                                if (!(matrix.ContainsKey(techniqueId) && matrix[techniqueId].Contains(category)))
-                                                    warnings.Add($"MITRE ATT&CK technique ({techniqueId}) and tactic ({category}) mismatch in rule: {ruleFile.Split("\\").Last()}");
-                                            }
-                                            catch (Exception)
-                                            {
-                                                // ignored
-                                            }
+                                            foreach(string category in categories)
+                                                if (!(mismatchSearchMatrix.ContainsKey(techniqueId) && mismatchSearchMatrix[techniqueId].Contains(category)))
+                                                    mismatchWarnings.Add($"MITRE ATT&CK technique ({techniqueId}) and tactic ({category}) mismatch in rule: {ruleFilePath.Split("\\").Last()}");
                                         }
                                     }
                                     else
                                     {
-                                        category = tag.Replace("attack.", "")
-                                        .Replace("_", "-")
-                                        .ToLower();
+                                        //if its the start of a new technique block, then clean categories and adds first category
+                                        if (lastEntry == null || lastEntry.StartsWith("attack.t"))
+                                            categories = new List<string>();
+                                        categories.Add(
+                                            tag.Replace("attack.", "")
+                                            .Replace("_", "-")
+                                            .ToLower());
                                     }
+                                    lastEntry = tag;
                                 }
                             }
                         }
                         catch (YamlException e)
                         {
-                            Console.Error.WriteLine($"Ignoring rule {ruleFile} (parsing failed)");
+                            Console.Error.WriteLine($"Ignoring rule {ruleFilePath} (parsing failed)");
                         }
                     }
-                    var entries = techs
+                    var entries = techniques
                         .ToList()
                         .Select(entry => new
                         {
@@ -136,30 +106,118 @@ namespace Sigma2AttackNet
                             score = entry.Value.Count,
                             comment = (o.NoComment) ? null : string.Join(Environment.NewLine, entry.Value.Select(x => x.Split("/").Last()))
                         });
-                    File.WriteAllText(o.OutFile.EndsWith(".json") ? o.OutFile : $"{o.OutFile}.json", JsonConvert.SerializeObject(new
-                    {
-                        domain = "mitre-enterprise",
-                        name = "Sigma rules coverage",
-                        gradient = new
-                        {
-                            colors = new[] { "#a0eab5", "#0f480f" },
-                            maxValue = gradientMax,
-                            minValue = 0
-                        },
-                        version = "4.1",
-                        techniques = entries
-                    }, Formatting.Indented, new JsonSerializerSettings
-                    {
-                        NullValueHandling = NullValueHandling.Ignore
-                    }));
-                    Console.WriteLine($"[*] Layer file written in {o.OutFile} ({ruleCount} rules)");
-                    if (warnings.Any())
-                    {
-                        Console.WriteLine(" ");
-                        Console.WriteLine("Attention - mismatch between technique and tactic has been detected!");
-                    }
-                    warnings.ForEach(Console.WriteLine);
+                    WriteToFile(o, gradientMax, ruleCount, entries);
+                    PrintWarnings();
                 });
+        }
+        /// <summary>
+        /// Prints config.json params and then adds default MITRE matrix url in case it's missing from options
+        /// </summary>
+        /// <param name="o"></param>
+        public static void LoadConfig(Options o)
+        {
+            //Write all the blah blah
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceStream = assembly.GetManifestResourceStream("Sigma2AttackNet.config.json");
+            StreamReader reader = new StreamReader(resourceStream);
+            var config = JsonConvert.DeserializeObject<JObject>(reader.ReadToEnd());
+            Console.WriteLine($"\n S2AN by 3CORESec - {config["repo_url"]}\n");
+            //Load default configuration for ATT&CK technique and tactic mismatch search
+            if (o.MitreMatrix == null)
+            {
+                o.MitreMatrix = config["category_matrix_url"]?.ToString();
+            }
+        }
+        /// <summary>
+        /// If warning flag in options is true, then mismatchSearchMatrix is loaded with information from the Mitre matrix url
+        /// </summary>
+        /// <param name="o"></param>
+        public static void LoadMismatchSearchMatrix(Options o)
+        {
+            if (o.Warning)
+            {
+                foreach (var x in (JsonConvert.DeserializeObject<JObject>(new WebClient().DownloadString(o.MitreMatrix))["objects"] as JArray)!
+                    .Where(x => x["external_references"] != null && x["external_references"].Any(y => y["source_name"] != null && x["kill_chain_phases"] != null)))
+                {
+                    var techId = x["external_references"]
+                        .First(x => x["source_name"].ToString() == "mitre-attack")["external_id"]
+                        .ToString();
+                    if (!mismatchSearchMatrix.ContainsKey(techId))
+                        mismatchSearchMatrix.Add(techId,
+                            x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString()).ToList()
+                        );
+                    else
+                    {
+                        mismatchSearchMatrix[techId] = mismatchSearchMatrix[techId].Concat(x["kill_chain_phases"]!.Select(x => x["phase_name"].ToString())).ToList();
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Prints warnings in mismatchWarnings
+        /// </summary>
+        public static void PrintWarnings()
+        {
+            if (mismatchWarnings.Any())
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Attention - mismatch between technique and tactic has been detected!");
+            }
+            mismatchWarnings.ForEach(Console.WriteLine);
+        }
+        /// <summary>
+        /// Writes entries to file
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="gradientMax"></param>
+        /// <param name="ruleCount"></param>
+        /// <param name="entries"></param>
+        public static void WriteToFile(Options o, int gradientMax, int ruleCount, IEnumerable<dynamic> entries)
+        {
+            try
+            {
+
+                File.WriteAllText(o.OutFile.EndsWith(".json") ? o.OutFile : $"{o.OutFile}.json", JsonConvert.SerializeObject(new
+                {
+                    domain = "mitre-enterprise",
+                    name = "Sigma rules coverage",
+                    gradient = new
+                    {
+                        colors = new[] { "#a0eab5", "#0f480f" },
+                        maxValue = gradientMax,
+                        minValue = 0
+                    },
+                    version = "4.1",
+                    techniques = entries
+                }, Formatting.Indented, new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                }));
+                Console.WriteLine($"[*] Layer file written in {o.OutFile} ({ruleCount} rules)");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Problem writing to file: " + e.Message);
+            }
+        }
+
+        public static Dictionary<string, dynamic> DeserializeYamlFile(string ruleFilePath, Options o)
+        {
+            var contents = File.ReadAllText(ruleFilePath);
+            if (!contents.Contains("tags"))
+            {
+                Console.WriteLine($"Ignoring rule {ruleFilePath} (no tags)");
+                return null;
+            }
+            if (o.Warning)
+                contents = contents.Replace(Environment.NewLine + Environment.NewLine,
+                        Environment.NewLine)
+                    .Remove(0, contents.IndexOf("tags", StringComparison.Ordinal));
+            if (contents.Contains("---"))
+                contents = contents.Remove(contents.IndexOf("---", StringComparison.Ordinal));
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
+            var dict = deserializer.Deserialize<Dictionary<string, dynamic>>(contents);
+            return dict;
         }
     }
 }
